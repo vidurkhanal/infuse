23c82bb6e49e62cb859a77c0632e6f9a4a22a979
vidurkhanal <vkhanal@caldwell.edu>
2024-01-17 22:16:29 -0500

    feat(WIP): added x-mode and x-config parsing

--- middlewares/request_validator.go
+++ middlewares/request_validator.go
@@ -2,10 +2,17 @@ package middlewares
 
 import (
 	"fmt"
+	"slices"
 
+	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/vidurkhanal/infuse/constants"
 )
+
+type HeaderConfig struct {
+	Provider string `json:"provider"`
+	Targets  string `json:"targets"`
+}
 
 func RequestValidator(c *fiber.Ctx) error {
 	if c.Get("content-type") == "" ||
@@ -17,10 +24,31 @@ func RequestValidator(c *fiber.Ctx) error {
 		})
 	}
 
-	if c.Get(fmt.Sprintf("x-%s-mode", constants.POWERED_BY)) == "" {
+	if (c.Get(fmt.Sprintf("x-%s-mode", constants.POWERED_BY)) == "") ||
+		!slices.Contains(constants.Providers,
+			c.Get(fmt.Sprintf("x-%s-mode", constants.POWERED_BY))) {
+
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"status":  "error",
-			"message": fmt.Sprintf("missing header: %s", fmt.Sprintf("x-%s-mode", constants.POWERED_BY)),
+			"message": fmt.Sprintf("missing or invalid header: %s", fmt.Sprintf("x-%s-mode", constants.POWERED_BY)),
+		})
+	}
+
+	if c.Get(fmt.Sprintf("x-%s-config", constants.POWERED_BY)) == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":  "error",
+			"message": fmt.Sprintf("missing header: %s", fmt.Sprintf("x-%s-config", constants.POWERED_BY)),
+		})
+	}
+
+	var headerConfig HeaderConfig
+	if err := json.Unmarshal([]byte(
+		c.Get(fmt.Sprintf("x-%s-config",
+			constants.POWERED_BY))), &headerConfig); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status": "error",
+			"message": fmt.Sprintf("%s must be a valid json",
+				fmt.Sprintf("x-%s-config", constants.POWERED_BY)),
 		})
 	}
 
