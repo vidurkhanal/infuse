1449e1b7a5458735208a8bcab882647ded8047b4
vidurkhanal <vkhanal@caldwell.edu>
2024-01-23 23:35:33 -0500

    fix: switched from map to fasthttp.Response

--- handlers/target.go
+++ handlers/target.go
@@ -1,15 +1,18 @@
 package handlers
 
 import (
+	"errors"
 	"fmt"
-	"math/rand"
+	"sync"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/valyala/fasthttp"
 	"github.com/vidurkhanal/infuse/config"
 	"github.com/vidurkhanal/infuse/constants"
+	"github.com/vidurkhanal/infuse/utils"
 )
 
-func TryTarget(c *fiber.Ctx, t config.Target, fn, method, path string) (fiber.Map, error) {
+func TryTarget(c *fiber.Ctx, t config.Target, fn, method, path string) (*fasthttp.Response, error) {
 	switch t.Strategy.Mode {
 	case constants.StrategyFallback:
 		// fallback
@@ -17,18 +20,32 @@ func TryTarget(c *fiber.Ctx, t config.Target, fn, method, path string) (fiber.Ma
 	case constants.StrategyLoadBalance:
 		// load balance
 		return LoadBalance(c, t, fn, method, path)
-	default:
+	case constants.StrategySingle:
 		// try single target
 		return SingleTarget(c, t, fn, method, path)
+	default:
+		return TryPost(c, t, fn, method, path)
 	}
 }
 
-func Fallback(c *fiber.Ctx, t config.Target, fn, method, path string) (fiber.Map, error) {
+func Fallback(c *fiber.Ctx, t config.Target, fn, method, path string) (*fasthttp.Response, error) {
 	// Todo
-	return nil, nil
+	var wg sync.WaitGroup
+	var res *fasthttp.Response
+	var err error
+	for target_indx, target := range t.Targets {
+		wg.Add(1)
+		currentPath := fmt.Sprintf("%s.targets[%d]", path, target_indx)
+		go func(t config.Target) {
+			defer wg.Done()
+			res, err = TryTarget(c, t, fn, method, currentPath)
+		}(target)
+	}
+	wg.Wait()
+	return res, err
 }
 
-func LoadBalance(c *fiber.Ctx, t config.Target, fn, method, path string) (fiber.Map, error) {
+func LoadBalance(c *fiber.Ctx, t config.Target, fn, method, path string) (*fasthttp.Response, error) {
 	var totalWeight int
 	for _, target := range t.Targets {
 		if target.Weight == 0 {
@@ -37,17 +54,22 @@ func LoadBalance(c *fiber.Ctx, t config.Target, fn, method, path string) (fiber.
 		totalWeight += target.Weight
 	}
 
-	randomWeight := rand.Float32() * float32(totalWeight)
+	randomWeight := utils.GetRngInstance().Gen.Intn(totalWeight + 1)
 	for target_indx, target := range t.Targets {
-		if float32(target.Weight) < randomWeight {
+		if target.Weight < randomWeight {
 			currentPath := fmt.Sprintf("%s.targets[%d]", path, target_indx)
-			return SingleTarget(c, target, fn, method, currentPath)
+			return TryTarget(c, target, fn, method, currentPath)
 		}
-		randomWeight -= float32(target.Weight)
+		randomWeight -= target.Weight
 	}
-	return nil, nil
+
+	return &fasthttp.Response{}, errors.New("no target found")
 }
 
-func SingleTarget(c *fiber.Ctx, t config.Target, fn, method, path string) (fiber.Map, error) {
-	return nil, nil
+func SingleTarget(c *fiber.Ctx, t config.Target, fn, method, path string) (*fasthttp.Response, error) {
+	return TryTarget(c, t.Targets[0], fn, method, path+".targets[0]")
+}
+
+func TryPost(c *fiber.Ctx, t config.Target, fn, method, path string) (*fasthttp.Response, error) {
+	return TryTarget(c, t, fn, fiber.MethodPost, path)
 }
