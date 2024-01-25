1160fcd34999216df33a6609a40a48e4d870dc14
vidurkhanal <vkhanal@caldwell.edu>
2024-01-25 12:43:14 -0500

    chore: moved to config/

--- providers/providers.go
+++
@@ -1,9 +0,0 @@
-package providers
-
-type ParameterConfig struct {
-	Param    string      `json:"param"`
-	Default  interface{} `json:"default"`
-	Min      int         `json:"min"`
-	Max      int         `json:"max"`
-	Required bool        `json:"required"`
-}
