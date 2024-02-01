2063a505164f199fcf89d125f869953492b46692
vidurkhanal <vkhanal@caldwell.edu>
2024-02-01 00:58:22 -0500

    fix: initialize providers before starting HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/vidurkhanal/infuse/cmd"
+import (
+	"github.com/vidurkhanal/infuse/cmd"
+	"github.com/vidurkhanal/infuse/config"
+)
 
 func main() {
+	_ = config.Providers()
 	cmd.NewApp().StartHTTPServer()
 }
